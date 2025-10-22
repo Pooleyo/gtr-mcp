24cddebae0d91f5331dcfaa396c5ba6b6623003a
Ashley Poole <ashley.john.poole@gmail.com>
2025-10-22 22:37:11 +0100

    Update response format

--- app/mcp_server.py
+++ app/mcp_server.py
@@ -7,13 +7,13 @@ mcp = FastMCP("gtr")
 
 @mcp.tool()
 async def get_projects(q: str):
-    """Get projects from the GtR API matching a query
+    """Get projects from the GtR API that match a query.
     
     Args:
-        q: A very short query that is used to detect matching keywords in the title, abstract or project reference.
+        q: A very query that is used to detect matching keywords in the title, abstract or project reference.
     """
-    response = await GtrClient.get_projects(q)
-    return response.json()
+    projects = await GtrClient.get_projects(q)
+    return projects
 
 def main():
     mcp.run(transport='stdio')
